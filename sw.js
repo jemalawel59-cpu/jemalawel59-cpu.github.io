77092c2a0e9a7af96618bc91b6df303c279ed74e
jemalawel59-cpu <jemalawel59@gmail.com>
2026-02-05 11:02:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,24 @@
+const CACHE_NAME = "mosque-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./mosque.jpg"
+];
+
 self.addEventListener("install", event => {
-  console.log("Service Worker installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
