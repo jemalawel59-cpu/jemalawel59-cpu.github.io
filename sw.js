f517d782b14a2cf94167bcdacc7a61deabb62305
jemalawel59-cpu <jemalawel59@gmail.com>
2026-02-08 10:41:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+const CACHE_NAME = "al-rahma-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/manifest.json",
+  "/mosque.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
